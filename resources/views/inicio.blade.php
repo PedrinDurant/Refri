bce0aa978c9d759d54a70cb7a22107d9e96061d5
PedroDurant <pedropduran16@gmail.com>
2024-03-29 14:06:36 -0400

    mi primer commit refrivent-laravel

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Refrivent</title>
+</head>
+<body>
+    
+    <h1>refrivent</h1>
+    <h2>refrivent</h2>
+    <h3>refrivent</h3>
+    <h4>refrivent</h4>
+    <h5>refrivent</h5>
+    <h5>refrivent</h5>
+</body>
+</html>
